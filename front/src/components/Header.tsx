159fccb7988053e3a8934b0a58b2517abb05ec69
augusto <augustomenezesd@gmail.com>
2023-11-08 15:39:30 -0300

    arrumando imports absolutos

---
+++ front/src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+    <>
+      <nav className="bg-primary h-[60px] w-full">
+        <div>
+            test
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary":"#14ff00",
+        "secondary":"#f6f6f6",
+        "tertiary": "#000",
+      },
+      fontFamily:{
+        
+      }
+    },
   },
   plugins: [],
 }
