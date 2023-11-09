068e083adfe1e96570c2fdce45879bfc5553e74d
augusto <augustomenezesd@gmail.com>
2023-11-09 16:15:38 -0300

    add footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "components/Header";
+import Footer from "components/Footer";
 import Home from "pages/Home";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             {/* <Route path="/about" component={About} /> */}
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </>
   );
