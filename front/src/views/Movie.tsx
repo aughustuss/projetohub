e0d6337dce48c871f7ce04c0183736f5d1c97009
MicaeOliveira-dev <micaeldev@hotmail.com>
2023-12-12 11:27:34 -0300

    Feat:  Componente de comentarios criado

--- front/src/views/Movie.tsx
+++ front/src/views/Movie.tsx
@@ -1,11 +1,20 @@
-import MovieInfo from "components/MovieInfo";
+import MovieInfo from "components/movieInfo";
+import CommentSection from "components/ComentSection";
 import { useParams } from "react-router-dom";
+import authorImage from '../assets/sobre.jpeg';
+
 
 const Movie = () => {
   const { movieId } = useParams();
+  const comments = [
+    { id: 1, author: 'Augusto Pereira', date: '2023-01-01', text: 'Muitoooo bommm!', likes: 10, profileImage: authorImage },
+    { id: 2, author: 'Micael Oliveira', date: '2023-01-02', text: 'Filme muito bom mesmo', likes: 5, profileImage: authorImage },
+    { id: 3, author: 'Carlos', date: '2023-01-03', text: 'esse filme é muito ruimm', likes: 2, profileImage: authorImage },
+  ];
   return (
     <>
       <MovieInfo movieId={movieId} />
+      <CommentSection comments={comments}/>
     </>
   )
     
